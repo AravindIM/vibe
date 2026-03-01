bec802cacd03a47f0483160afe367fe836bb7c1e
Aravind I M <aravindim@yahoo.com>
2026-03-01 18:18:33 +0530

    feat(web): new file menu

--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-	"sync"
+	"fmt"
+	"net/http"
 
 	webview "github.com/AravindIM/webview_go"
+	"github.com/gin-gonic/gin"
 )
 
-const address = "http://localhost:8080/"
+const address = "localhost:8080"
 
-func web(wg *sync.WaitGroup) {
-	defer wg.Done()
+func web(done chan<- error) {
 	w := webview.New(false)
 	defer w.Destroy()
 	w.SetTitle("Vibe")
 	w.SetSize(480, 320, webview.HintNone)
-	w.Navigate(address)
+	w.Navigate(fmt.Sprintf("http://%s", address))
 	w.Run()
+	done <- fmt.Errorf("Closed")
+}
+
+func api(done chan<- error) {
+	count := 0
+	router := gin.Default()
+	router.LoadHTMLFiles("./template/index.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"count": fmt.Sprintf("%d", count)})
+	})
+	router.POST("/add", func(c *gin.Context) {
+		count += 1
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(fmt.Sprintf("%d", count)))
+	})
+	router.POST("/sub", func(c *gin.Context) {
+		count -= 1
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(fmt.Sprintf("%d", count)))
+	})
+	done <- router.Run(address)
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go web(&wg)
-	wg.Wait()
+	done := make(chan error)
+	go web(done)
+	go api(done)
+	err := <-done
+	fmt.Println("Exiting:", err)
 }
