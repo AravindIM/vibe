d8b6943c1af8931819ed6cb4d89db5d73bdcee9d
Aravind I M <aravindim@yahoo.com>
2026-03-01 10:38:52 +0530

    feat(web): added webview ui component

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"sync"
+
+	webview "github.com/AravindIM/webview_go"
+)
+
+const address = "http://localhost:8080/"
+
+func web(wg *sync.WaitGroup) {
+	defer wg.Done()
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("Vibe")
+	w.SetSize(480, 320, webview.HintNone)
+	w.Navigate(address)
+	w.Run()
+}
 
 func main() {
-	fmt.Print("Welcome to Vibe!\n")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go web(&wg)
+	wg.Wait()
 }
